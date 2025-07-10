88407813f33d2f0b889422c3abad30fe394e2667
Melissa Gandy <MSGAND01@louisville.edu>
2025-07-09 22:45:54 -0400

    fixes

--- electron/main.js
+++ electron/main.js
@@ -1,12 +1,40 @@
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('path');
+const { execFile } = require('child_process');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1000,
+    height: 800,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: true
+    }
+  });
+
+  win.loadFile(path.join(__dirname, 'app', 'index.html'));
+}
+
+// Handle IPC call from renderer to run C++ exe
 ipcMain.handle('run-medication-manager', async () => {
   return new Promise((resolve, reject) => {
     const exePath = path.join(__dirname, 'app', 'MedicationManager.exe');
     execFile(exePath, (error, stdout, stderr) => {
       if (error) {
-        reject(stderr);
+        reject(stderr || error.message);
       } else {
         resolve(stdout);
       }
     });
   });
+});
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
 });
